52b7ecb1bb84fb2e0cc1be153dec400cfe7447e1
Fariya <fariya.webdev@gmail.com>
2024-12-14 17:06:54 +0600

    feat: get jobs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -13,35 +13,48 @@ console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
 
+        const jobCollection = client.db("jobPortal").collection("jobs")
 
+        app.get('/jobs', async (req, res) => {
+            const cursor = jobCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
 
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.get('/jobs/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = { _id : new ObjectId(id)}
+            const result = await jobCollection.findOne(query)
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Jop portal server')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server running at port: ${port}`)
 })
